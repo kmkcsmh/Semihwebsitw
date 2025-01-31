35632de3e092f4648fe1a88960d68fc04b1b011b
kmkcsmh <101885252+kmkcsmh@users.noreply.github.com>
2025-02-01 01:09:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function searchGoogle() {
+    let query = document.getElementById('search-box').value;
+    if (query) {
+        window.location.href = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+    }
+}
